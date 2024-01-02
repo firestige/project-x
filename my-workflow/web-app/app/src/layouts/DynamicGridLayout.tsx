7a3bb37c05b4a03d6236509c950b6a10eb39a02d
firestige <zzt.nbl@hotmail.com>
2024-01-03 02:23:09 +0800

    更新网格布局

--- my-workflow/web-app/app/src/layouts/DynamicGridLayout.tsx
+++ my-workflow/web-app/app/src/layouts/DynamicGridLayout.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Responsive, WidthProvider } from 'react-grid-layout'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 
 const ResponsiveGridLayout = WidthProvider(Responsive)
 

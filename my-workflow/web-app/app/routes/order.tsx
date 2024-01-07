e59d8a22733400a4aa4b44f34d3d92d57a234425
firestige <zzt.nbl@hotmail.com>
2024-01-08 03:14:03 +0800

    切换echart

--- my-workflow/web-app/app/routes/order.tsx
+++ my-workflow/web-app/app/routes/order.tsx
@@ -1,5 +1,28 @@
+import LineChart, { EChartsOption } from '~/src/components/charts/LineChart'
+
+const options = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line',
+    },
+  ],
+} as EChartsOption
+
 const Order = () => {
-  return <h1>订单</h1>
+  return (
+    <>
+      <h1>订单</h1>
+      <LineChart options={options} />
+    </>
+  )
 }
 
 export default Order
--- my-workflow/web-app/app/src/components/charts/LineChart.tsx
+++ my-workflow/web-app/app/src/components/charts/LineChart.tsx
@@ -1,9 +1,28 @@
-import * as d3 from 'd3'
+import { Box } from '@mui/material'
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
 
-export interface LinerProps {}
+export type EChartsOption = echarts.EChartsOption
 
-const LineGraph = () => {
-  return <></>
+export interface LinerProps {
+  options: EChartsOption
+}
+
+const LineGraph: React.FC<LinerProps> = (props) => {
+  const { options } = props
+  const ref = useRef<HTMLDivElement>(null)
+  // const chart = useMemo(() => ref && echarts.init(ref.current), [])
+  useEffect(() => {
+    console.log(options)
+    let _chart = echarts.getInstanceByDom(ref.current!)
+    if (!_chart) {
+      console.log(_chart)
+      _chart = echarts.init(ref.current)
+    }
+    options && _chart.setOption(options)
+  }, [options])
+
+  return <Box sx={{ height: '80vh' }} ref={ref} />
 }
 
 export default LineGraph
